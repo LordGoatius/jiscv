4eb430979fd7e7066c8a6407eb1e6b638b2ac9ad
Jimmy Ostler <jtostler1@gmail.com>
2025-12-24 02:35:39 -0800

    try v2 learning

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,6 +2,8 @@
 #![feature(try_trait_v2, try_trait_v2_residual)]
 
 pub mod syscall {
+    use core::ops::{ControlFlow, FromResidual, Try};
+
     pub mod consts {
         pub const SYS_PUTCHAR: usize = 1;
         pub const SYS_GETCHAR: usize = 2;
@@ -30,6 +32,30 @@ pub mod syscall {
         NotFound = -1,
         BufferSize = -2,
     }
+
+    impl Try for SyscallResult {
+        type Output = SysOk;
+
+        // May need to refactor for `SyscallResult<Infalliable, SysErr>` when made generic
+        type Residual = SysErr;
+
+        fn from_output(output: Self::Output) -> SyscallResult {
+            SyscallResult::Ok(output)
+        }
+
+        fn branch(self) -> ControlFlow<Self::Residual, Self::Output> {
+            match self {
+                SyscallResult::Ok(val) => ControlFlow::Continue(val),
+                SyscallResult::Err(err) => ControlFlow::Break(err),
+            }
+        }
+    }
+
+    impl FromResidual<SysErr> for SyscallResult {
+        fn from_residual(residual: SysErr) -> SyscallResult {
+            SyscallResult::Err(residual)
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -42,7 +68,6 @@ pub enum FileResult {
 #[derive(Debug)]
 #[repr(usize)]
 pub enum FileErr {
-    FileNotFound, 
+    FileNotFound,
     BufferTooLarge
 }
-
